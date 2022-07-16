a3d2809c892d69b0a95d61b11329ea6cbbaecde7
t-leyangxue <t-leyangxue@microsoft.com>
2022-07-16 22:25:25 +0000

    arima

--- tests/test_autoarima.py
+++ tests/test_autoarima.py
@@ -1,33 +1,86 @@
+from dataclasses import dataclass, field
 import os
+
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 from pmdarima.arima import auto_arima
 import pandas as pd
 
 from forecast.data.loader import DataParser
 
 from sklearnex import patch_sklearn
-patch_sklearn()
 
-# folder = "directed-abilene-zhang-5min-over-6months-ALL"
-folder = "directed-geant-uhlig-15min-over-4months-ALL"
+patch_sklearn()
+from forecast.data.loader import DataParser
+from forecast.data.prophet import ProphetDataParser
+from forecast.utils.cmdparser import HfArgumentParser
+
+
+@dataclass
+class ModelConfig:
+    folder: str = field(metadata={"help": "folder for xml data"})
+    checkpoint: str = field(metadata={"help": "path to checkpoints"})
+
+    def __post_init__(self):
+        try:
+            os.mkdir(self.checkpoint)
+        except:
+            pass
+
+
+parser = HfArgumentParser(ModelConfig)
+args = parser.parse_args_into_dataclasses()[0]
 
 parser = DataParser()
-df = parser.parse_sndlib_xml(folder)
-
-# df = pd.read_csv(os.path.join(folder, filename))
+df = parser.parse_sndlib_xml(args.folder)
 df = df.fillna(0)
+
+print(df.head())
+
 df_len = len(df.index.values)
 train_len = int(df_len * 0.6)
 
-df_train = df.loc[:train_len]
-df_test = df.loc[train_len:]
+dataset = df.drop(columns=["timestamps"]).values
+training_set = dataset[:train_len]
+test_set = dataset[train_len:]
 
-node_pairs = df_train.columns[:-1]
-print(node_pairs)
 
-model = auto_arima(df_train[node_pairs[0]], df_train[node_pairs[1:]].to_numpy())
-# model.fit(df_train[node_pairs[3]])
-forecasts, conf_int = model.predict(len(df_test.index.values), X=df_test[node_pairs[1:]].to_numpy(), return_conf_int=True)
+columns = list(df.columns)[:-1]
+forecast_list = []
+lb_list = []
+ub_list = []
+for i, col in enumerate(columns):
+    data = training_set[:, i]
+    sc = MinMaxScaler(feature_range=(0, 1))
+    training_set_scaled = sc.fit_transform(data.reshape(-1, 1))
 
-print(forecasts)
-print(forecasts - df_test.index.values)
-print(conf_int)
+    model = auto_arima(training_set_scaled)
+    forecasts, conf_int = model.predict(
+        df_len-train_len,
+        return_conf_int=True,
+    )
+
+    forecasts = sc.inverse_transform(forecasts.reshape(-1, 1))
+    lower_bound = sc.inverse_transform(conf_int[0,:].reshape(-1, 1))
+    upper_bound = sc.inverse_transform(conf_int[1,:].reshape(-1, 1))
+    forecast_list.append(forecasts.tolist())
+    lb_list.append(lower_bound.tolist())
+    ub_list.append(upper_bound.tolist())
+
+    np.save(os.path.join(args.checkpoint, "test.npy"), test_set, allow_pickle=False)
+    np.save(
+        os.path.join(args.checkpoint, "pred.npy"),
+        np.array(forecast_list),
+        allow_pickle=False,
+    )
+
+    np.save(
+        os.path.join(args.checkpoint, "pred_lb.npy"),
+        np.array(lb_list),
+        allow_pickle=False,
+    )
+    np.save(
+        os.path.join(args.checkpoint, "pred_ub.npy"),
+        np.array(ub_list),
+        allow_pickle=False,
+    )
